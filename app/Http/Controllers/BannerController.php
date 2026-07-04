67901e18b14a173bdb46daed01ae6b5120d2c6fd
jofanrizks <jofanrizks@gmail.com>
2026-07-04 20:02:12 +0700

    fix banner

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Banner;
 use Illuminate\Http\Request;
+use App\Models\ActivityLog;
 
 class BannerController extends Controller
 {
@@ -21,27 +22,54 @@ class BannerController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-        'title' => 'nullable|string',
-        'images' => 'required|array',
-        'images.*' => 'image|mimes:jpg,jpeg,png|max:2048'
-    ]);
+            'title' => 'required|string|in:Banner 1,Banner 2,Banner 3,Banner 4,Banner 5',
+            'images' => 'required|array|max:1',
+            'images.*' => 'image|mimes:jpg,jpeg,png|max:2048'
+        ]);
 
-        $banners = [];
+        $user = auth()->user();
 
-        foreach ($request->file('images') as $image) {
+        $image = $request->file('images')[0];
 
-            $path = $image->store('banner', 'public');
+        $path = $image->store('banner', 'public');
 
-            $banners[] = Banner::create([
+        $banner = Banner::where('title', $request->title)->first();
+
+        if ($banner) {
+
+            // hapus gambar lama
+            if (
+                $banner->image &&
+                \Storage::disk('public')->exists($banner->image)
+            ) {
+                \Storage::disk('public')->delete($banner->image);
+            }
+
+            $banner->update([
+                'image' => $path,
+                'is_active' => true,
+            ]);
+
+        } else {
+
+            $banner = Banner::create([
                 'title' => $request->title,
                 'image' => $path,
                 'is_active' => true,
             ]);
+
         }
+
+        ActivityLog::create([
+            'user_id' => $user->id,
+            'activity' => 'Add Banner',
+            'description' => "Mengubah {$request->title}",
+            'ip_address' => $request->ip(),
+        ]);
 
         return response()->json([
             'success' => true,
-            'data' => $banners,
+            'data' => $banner,
         ]);
     }
 
@@ -50,6 +78,12 @@ class BannerController extends Controller
         Banner::findOrFail($id)
             ->delete();
 
+        ActivityLog::create([
+            'user_id' => $user->id,
+            'activity' => 'Delete Banner',
+            'description' => 'User menghapus banner',
+            'ip_address' => $request->ip(),
+        ]);
         return response()->json([
             'success' => true
         ]);
